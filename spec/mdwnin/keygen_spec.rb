a5fae2f371b5594f728a0c9ad79e0fe4e1b508d2
madx <madx+github@yapok.org>
2013-05-14 08:20:54 +0200

    Add some tests for the Keygen class

--- spec/mdwnin/keygen_spec.rb
+++ spec/mdwnin/keygen_spec.rb
@@ -6,6 +6,7 @@ require 'mdwnin/keygen'
 
 describe Mdwnin::Keygen do
   subject { Mdwnin::Keygen }
+  let(:time) { Time.at(42) }
 
   describe ".generate" do
     it "returns a non empty string" do
@@ -17,6 +18,19 @@ describe Mdwnin::Keygen do
 
       key.must_match /^[a-z0-9.]+$/
       key.count('.').must_equal 1
+    end
+
+    it "takes an optional base time" do
+      key = subject.generate(time)
+
+      key.must_match time.usec.to_s(36)
+    end
+
+    it "does not return the same key for two runs atthe same time" do
+      key1 = subject.generate(time)
+      key2 = subject.generate(time)
+
+      key1.wont_equal key2
     end
   end
 end
