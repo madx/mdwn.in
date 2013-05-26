3419c0fadb28323fb09b5443ce1f75b1531c81d2
madx <madx+github@yapok.org>
2013-05-26 14:50:21 +0200

    Unicorn config for the development environment

--- config/unicorn.example.rb
+++ config/unicorn.example.rb
@@ -1,19 +1,23 @@
 # config/unicorn.rb
 
+environment = ENV["RACK_ENV"] || "development"
+
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 30
 preload_app true
 
-app_dir = "" # == set this to the deploy_to value in the capistrano recipe
+if "production" == environment
+  app_dir = "" # == set this to the deploy_to value in the capistrano recipe
 
-working_directory File.join(app_dir, "current")
-shared_path = File.join(app_dir, "shared")
+  working_directory File.join(app_dir, "current")
+  shared_path = File.join(app_dir, "shared")
 
-listen "127.0.0.1:3402"
-pid "#{shared_path}/pids/unicorn.pid"
+  listen "127.0.0.1:3402"
+  pid "#{shared_path}/pids/unicorn.pid"
 
-stderr_path "#{shared_path}/log/unicorn.stderr.log"
-stdout_path "#{shared_path}/log/unicorn.stdout.log"
+  stderr_path "#{shared_path}/log/unicorn.stderr.log"
+  stdout_path "#{shared_path}/log/unicorn.stdout.log"
+end
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
