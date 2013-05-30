a6c19e3fd5ee26c173311b9d219068dfb8af7e91
madx <madx+github@yapok.org>
2013-05-30 08:52:48 +0200

    Update deploy recipes

---
+++ config/deploy.example.rb
@@ -0,0 +1,49 @@
+require "bundler/capistrano"
+
+set :application, "mdwn.in"
+set :repository,  "git://github.com/madx/mdwn.in.git"
+
+set :scm, "git"
+set :use_sudo, false
+
+set :deploy_via, :remote_cache
+set :ssh_options, { forward_agent: true }
+
+# Set your deploy user and deploy directory
+set :user, "YOUR DEPLOY USER"
+set :deploy_to, "DEPLOY DIRECTORY"
+
+# Set your server address
+role :web, "example.com"
+role :db,  "example.com", :primary => true
+
+set :keep_releases, 3
+after "deploy:restart", "deploy:cleanup"
+
+set :unicorn_pid, "#{fetch(:shared_path)}/pids/unicorn.pid"
+set :unicorn_sock, "#{fetch(:shared_path)}/sockets/unicorn.sock"
+set :unicorn_conf, "#{fetch(:release_path)}/config/unicorn.rb"
+
+set :bundle_flags, "--deployment --quiet --binstubs"
+
+# If you are using rbenv, modify an uncomment the following lines
+# set :default_environment, {
+#   'PATH' => "SHIMS_PATH:BIN_PATH:$PATH"
+# }
+
+# Unicorn control tasks
+namespace :deploy do
+  task :restart do
+    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+end
+
+after 'deploy:update_code' do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
---
+++ config/unicorn.example.rb
@@ -0,0 +1,31 @@
+# config/unicorn.rb
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 30
+preload_app true
+
+app_dir = "" # == set this to the deploy_to value in the capistrano recipe
+
+working_directory File.join(app_dir, "current")
+shared_path = File.join(app_dir, "shared")
+
+listen "127.0.0.1:3402"
+pid "#{shared_path}/pids/unicorn.pid"
+
+stderr_path "#{shared_path}/logs/unicorn.stderr.log"
+stdout_path "#{shared_path}/logs/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts "Unicorn master intercepting TERM and sending myself QUIT instead"
+    Process.kill "QUIT", Process.pid
+  end
+
+  defined?(DB) and DB.disconnect
+end
+
+after_fork do |server, worker|
+  Signal.trap "TERM" do
+    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
+  end
+end
